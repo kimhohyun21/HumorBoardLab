22013aa13d803978bd94e273662af90d1f467586
parkj7701 <parkj7701@naver.com>
2016-09-28 11:54:10 +0900

    # WARNING: head commit changed in the meantime
    update

--- src/com/dao/BoardDAO.java
+++ src/com/dao/BoardDAO.java
@@ -7,7 +7,7 @@ import java.sql.*;
 public class BoardDAO {
 	private Connection conn;
 	private PreparedStatement ps;
-	private final String URL = "jdbc:oracle:thin:@211.238.142.83:1521:ORCL";
+	private final String URL = "jdbc:oracle:thin:@211.238.142.88:1521:ORCL";
 	
 	public BoardDAO(){
 		try{
@@ -19,7 +19,7 @@ public class BoardDAO {
 	
 	public void getConnection(){
 		try{
-			conn = DriverManager.getConnection(URL, "scott", "tiger");
+			conn = DriverManager.getConnection(URL, "scott", "1234");
 		}catch(Exception ex){
 			System.out.println(ex.getMessage());
 		}
---
+++ src/com/model/ContentModel.java
@@ -0,0 +1,21 @@
+package com.model;
+
+import javax.servlet.http.*;
+import com.dao.*;
+
+public class ContentModel implements Model{
+
+	@Override
+	public String handlerRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		String strNo = request.getParameter("no");
+		String strPage = request.getParameter("page");
+		
+		BoardDAO dao = new BoardDAO();
+		//boardContentData의 String 값을 Int 값으로 변환
+		BoardDTO dto = dao.boardCountData(Integer.parseInt(strNo))
+		
+		return "board/content.jsp";
+	}
+	
+}
